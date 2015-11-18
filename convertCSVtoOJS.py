db5a996612a7915d57b773ee0e1cff71fa5ecaa0
Mike Waugh <mwaugh2@lsu.edu>
2015-11-18 13:34:46 -0600

    converting review to base64

--- convertCSVtoOJS.py
+++ convertCSVtoOJS.py
@@ -1,15 +1,16 @@
 import csv
+import base64
 import xml.etree.ElementTree as ET
 
 with open ('cwbr.csv', 'r') as csvfile:
     reader = csv.DictReader(csvfile, delimiter='|')
     csvData = [blah for blah in reader]
-    issueDates = []
-    rows = []
-    for row in csvData:
-        issueDates.append(row['Issue_date'])
-        rows.append(row)
-    issueDates = set(issueDates)
+    issueDates = ["Fall 1999", "Summer 2007", "Winter 2013"]
+    #rows = []
+    #for row in csvData:
+    #    issueDates.append(row['Issue_date'])
+    #    rows.append(row)
+    #issueDates = set(issueDates)
 
     issues = {}
 
@@ -18,26 +19,18 @@ with open ('cwbr.csv', 'r') as csvfile:
 
     for arts in csvData:
         issueDate = arts['Issue_date']
-        issues[issueDate][arts['ID']]=arts
-        
-    def issuesxml(x):
-        result = []
-        for issue in x:
-            result.append(E("issue", issue))
-        return result
+        if issueDate in issueDates:
+            issues[issueDate][arts['ID']]=arts
 
+    for key, value in issues.items():
+        for k, art in value.items():
+            art["Review"]=base64.b64encode(bytes(str(art["Review"]), 'utf-8'))
+            
     xmlout = ET.Element("xmlout")
     for i in issues:
         ET.SubElement(xmlout, i)
 
-    print (len(xmlout))
-       # E.issues(
-        #    for issue in issues:
-         #       E.issue(
-          #      )
-        #)
-     #)   
-    #print(etree.tostring(xmloutput, pretty_print=True))
+
 
        
     
